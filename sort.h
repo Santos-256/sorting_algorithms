d04c427019b850d778b39ea8589c5007031cd199
Santos-256 <santosedward1998@gmail.com>
2024-01-23 03:05:56 -0800

    update

--- sort.h
+++ sort.h
@@ -40,8 +40,8 @@ void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 
 /* Sorting algoritms prototypes */
-void bubble_sort(int *, size_t);
-void insertion_sort_list(listint_t **);
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
